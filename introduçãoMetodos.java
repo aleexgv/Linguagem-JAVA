354a415cf4cdf6804644cd9b3fdec6e3479ddb5d
Alex Galhardo <aleexgvieira@gmail.com>
2017-06-04 16:31:16 -0300

    Create introduçãoMetodos.java

---
+++ introduçãoMetodos.java
@@ -0,0 +1,30 @@
+package exercicios;
+
+import java.util.*;
+
+public class Metodos {
+    
+    public static void main(String[] args){
+        
+        double areaRetangulo = retangulo();
+        
+        System.out.println("Area é: " + areaRetangulo);
+    }
+    
+    /* criando um novo metodo, fora do metodo main */
+    /* praticamente, semelhante a função vista em outras linguagens */
+    public static double retangulo(){
+        
+        Scanner input = new Scanner(System.in);
+        double base, altura, areaRetangulo =0;
+          
+        System.out.println("Informe a base do retangulo: ");
+        base = input.nextDouble();
+        System.out.println("Informe a altura do retangulo: ");
+        altura = input.nextDouble();
+        areaRetangulo = base * altura;
+        //System.out.println("A area do retangulo é: " + areaRetangulo);    
+        
+        return areaRetangulo;
+    }
+}
