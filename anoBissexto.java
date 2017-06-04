ef3f2ef037164222b95b25ebb446678347790cc1
Alex Galhardo <aleexgvieira@gmail.com>
2017-06-04 14:43:15 -0300

    Create anoBissexto.java

---
+++ anoBissexto.java
@@ -0,0 +1,22 @@
+
+package exercicios;
+
+import java.util.*;
+
+public class Bissexto {
+    
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Insira um ano: ");
+        int ano = input.nextInt();
+        
+        if((ano % 400==0) || (ano % 4 == 0 && ano%100 !=0)){
+            System.out.println("BISSEXTO");
+        }else{
+            System.out.println("NÃO BISSEXTO!");
+        }
+    }
+    
+}
